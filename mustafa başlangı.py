ef547dae1a8bf28d15a0bb1ed3b82defed394540
Mustafa <slagedon341@gmail.com>
2016-09-01 13:41:19 +0300

    check if username is ascii

--- mustafa başlangı.py
+++ mustafa başlangı.py
@@ -1,8 +1,21 @@
+
+def is_ascii(text):
+    try:
+        text.encode('ascii')
+        return True
+    except:
+        return False
+
+
 print("lütfen kullanıcı adınızı ve parolanızı belirleyiniz!!!"
       "(kullanıcı adı ve parolanızın karakter toplamı 30 u geçmemelidir")
 
 kullanıcı_adı = input("kullanıcı adınızı giriniz : \t")
 parola =("\nparolanızı giriniz : \t")
+
+if not is_ascii(kullanıcı_adı):
+    print('Lutfen turkce karakter kullanmayın')
+    quit()
 
 print("========================================"
       "TURGUT MOTOR BAKIM SERVİSİNE HOŞGELDİNİZ"

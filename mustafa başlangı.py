a9e47f84237ff8c4a5f6bbabb55eb891c6d80372
Mustafa <slagedon341@gmail.com>
2016-09-01 14:52:25 +0300

    fix space and indentation

--- mustafa başlangı.py
+++ mustafa başlangı.py
@@ -1,29 +1,46 @@
-
-def is_ascii(text):
-    try:
-        text.encode('ascii')
-        return True
-    except:
-        return False
-
-
-print("lütfen kullanıcı adınızı ve parolanızı belirleyiniz!!!"
-      "(kullanıcı adı ve parolanızın karakter toplamı 30 u geçmemelidir")
-
-kullanıcı_adı = input("kullanıcı adınızı giriniz : \t")
-parola = ("\nparolanızı giriniz : \t")
+while True:
+    def is_ascii(text):
+        try:
+            text.encode('ascii')
+            return True
+        except:
+            return False
 
 
-if not is_ascii(kullanıcı_adı):
-    print('Lutfen turkce karakter kullanmayın')
-    quit()
+    print("lütfen kullanıcı adınızı ve parolanızı belirleyiniz!!!\n"
+          "(kullanıcı adı ve parolanızda turkce karakter kullanmayınız\n")
 
-print("========================================"
-      "TURGUT MOTOR BAKIM SERVİSİNE HOŞGELDİNİZ"
-      "========================================"
-      "YAPMAK İSTEDİĞİNİZ İŞLEMİ SEÇİNİZ:"
-      "1) motor bakım servisi"
-      "2) motor yedek parça"
-      "3) elimizdeki parçalar"
-      "4) çıkış"
-      "========================================\n")
+    kullanıcı_adı = input("kullanıcı adınızı giriniz : \t")
+    parola = input("\nparolanızı giriniz : \t")
+
+
+    if not is_ascii(kullanıcı_adı):
+        print('Lutfen turkce karakter kullanmayın\n')
+        quit()
+    if not is_ascii(parola):
+        print("lutfen turkce karakter kullanmayın\n")
+        quit()
+
+
+    print("========================================\n"
+          "TURGUT MOTOR BAKIM SERVİSİNE HOŞGELDİNİZ\n"
+          "========================================\n"
+          "1) motor bakım servisi\n"
+          "2) motor yedek parça servisi\n"
+          "3) elimizdeki parçalar\n"
+          "4) çıkış\n"
+          "========================================\n")
+    seçim = input("YAPMAK İSTEDİĞİNİZ İŞLEMİ SEÇİNİZ:   \n")
+    if seçim == "1":
+        file1 = open("file1.txt", encoding='utf-8')
+        print(file1.read())
+        file1.close()
+    elif seçim == "3":
+        file2 = open("file2.txt",encoding='utf-8')
+        print(file2.read())
+        file2.close()
+    elif seçim == "2":
+        file3 = open("file3.txt",encoding='utf-8')
+        print(file3.read())
+        file3.close()
+

25c379424167498ce19cfb3e7d95936099ba46bc
Mustafa <slagedon341@gmail.com>
2016-09-01 13:27:07 +0300

    add program file

---
+++ mustafa başlangı.py
@@ -0,0 +1,18 @@
+print("lütfen kullanıcı adınızı ve parolanızı belirleyiniz!!!"
+      "(kullanıcı adı ve parolanızın karakter toplamı 30 u geçmemelidir")
+
+kullanıcı_adı = input("kullanıcı adınızı giriniz : \t")
+parola =("\nparolanızı giriniz : \t")
+
+print("========================================"
+      "TURGUT MOTOR BAKIM SERVİSİNE HOŞGELDİNİZ"
+      "========================================"
+      "YAPMAK İSTEDİĞİNİZ İŞLEMİ SEÇİNİZ:"
+      "1) motor bakım servisi"
+      "2) motor yedek parça"
+      "3) elimizdeki parçalar"
+      "4) çıkış"
+      "========================================\n")
+
+
+
